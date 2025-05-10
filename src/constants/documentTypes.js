21c783b78738168f248b1d40d7a8589846086d63
Berfin Semiz <senin-emailin@example.com>
2025-05-10 11:54:23 +0300

    Belge türü eklendi

--- src/constants/documentTypes.js
+++ src/constants/documentTypes.js
@@ -1,11 +1,14 @@
 const DocumentTypes = [
-    { label: 'Statik Proje', value: 'STATIK_PROJE' },
-    { label: 'Çağrı Mektubu', value: 'CAGRI_MEKTUBU' },
-    { label: 'Bağlantı Anlaşması', value: 'BAGLANTI_ANLASMASI' },
-    { label: 'GES Elektrik Proje', value: 'GES_ELEKTRIK_PROJE' },
-    { label: 'Belediye Uygunluk', value: 'BELEDIYE_UYGUNLUK' },
-    { label: 'Ön Kabul', value: 'ON_KABUL' },
-    { label: 'Geçici Kabul', value: 'GECICI_KABUL' }
+    { value: 'STATIK_PROJE', label: 'Statik Proje'  },
+    { value: 'CAGRI_MEKTUBU', label: 'Çağrı Mektubu'  },
+    { value: 'BAGLANTI_ANLASMASI', label: 'Bağlantı Anlaşması'  },
+    { value: 'GES_ELEKTRIK_PROJE', label: 'GES Elektrik Proje'  },
+    { value: 'BELEDIYE_UYGUNLUK', label: 'Belediye Uygunluk'  },
+    { value: 'ON_KABUL', label: 'Ön Kabul'  },
+    { value: 'GECICI_KABUL_EMIR_YAZISI', label: 'Geçici Kabul Emir Yazısı'  },
+    { value: 'KABUL_UST_YAZISI', label: 'Kabul Üst Yazısı'  },
+    { value: 'KABUL_TESLIM_TUTANAĞI', label: 'Kabul Teslim Tutanağı'  }
+
   ];
   
   export default DocumentTypes;
