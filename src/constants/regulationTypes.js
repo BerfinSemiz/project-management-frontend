56873f5aca343188db26f8c21fb7801d75f26882
Berfin Semiz <senin-emailin@example.com>
2025-05-10 14:31:09 +0300

    .

---
+++ src/constants/regulationTypes.js
@@ -0,0 +1,8 @@
+const RegulationTypes = [
+  { value: "ON_BIR_BIR", label: "11/1" },
+  { value: "ON_BIR_IKI", label: "11/2" },
+  { value: "ON_BIR_UC", label: "11/3" },
+  { value: "BES_BIR_H", label: "5.1h" },
+];
+
+export default RegulationTypes;
